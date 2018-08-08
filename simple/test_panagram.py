df60098faeeda9652eb2cd8ce47ac8abeee98b5e
Noufal Ibrahim <noufal@nibrahim.net.in>
2018-08-08 16:13:33 +0530

    RED: Adds test for mixed case

--- simple/test_panagram.py
+++ simple/test_panagram.py
@@ -8,4 +8,6 @@ def test_panagram_true():
     sentence = "the quick brown fox jumps over the lazy dog" 
     assert exercise.panagram(sentence) == True
     
-    
+def test_panagram_false_mixed_case():
+    sentence = "The quick brown fox jumped over the lazy dog" # s is missing
+    assert exercise.panagram(sentence) == False
