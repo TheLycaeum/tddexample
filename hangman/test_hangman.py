117ddcfc5a5ebcf994c61f62c419a20137994f8f
Noufal Ibrahim <noufal@nibrahim.net.in>
2018-08-08 16:28:38 +0530

    RED: Multiple words fails

--- hangman/test_hangman.py
+++ hangman/test_hangman.py
@@ -5,4 +5,10 @@ def test_select_secret_word():
     assert len(word) >= 6
     assert word.islower()
     assert word.isalpha()
+
+def test_select_multiple_secret_words():
+    words = set()
+    for i in range(10):
+        words.add(hangman.get_secret_word())
+    assert len(words) == 10
     
