35a9a8019797776987eb39fbcf0728f92c91cf16
Noufal Ibrahim <noufal@nibrahim.net.in>
2018-08-08 16:21:14 +0530

    GREEN: Adds test for punctuation

--- simple/test_panagram.py
+++ simple/test_panagram.py
@@ -19,3 +19,7 @@ def test_panagram_true_mixed_case():
 def test_panagram_false_with_punctuation():
     sentence = "The quick brown fox jumped over the lazy dog." # s is missing
     assert exercise.panagram(sentence) == False
+
+def test_panagram_true_with_punctuation():
+    sentence = "The quick brown fox jumps over the lazy dog."
+    assert exercise.panagram(sentence) == True
