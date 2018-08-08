506b49a251ccb7c54b49a65773cf4ddd55a5d4f3
Noufal Ibrahim <noufal@nibrahim.net.in>
2018-08-08 16:10:08 +0530

    RED: second test fails

--- simple/test_panagram.py
+++ simple/test_panagram.py
@@ -4,5 +4,8 @@ def test_panagram_false():
     sentence = "the quick brown fox jumped over the lazy dog" # s is missing
     assert exercise.panagram(sentence) == False
 
+def test_panagram_true():
+    sentence = "the quick brown fox jumps over the lazy dog" 
+    assert exercise.panagram(sentence) == True
     
     
